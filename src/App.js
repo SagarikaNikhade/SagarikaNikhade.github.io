602031a5cfc2b6f1c42413e1762c7c3f046ba561
wave2211 <sagarikanikhade073@gmail.com>
2023-03-21 22:18:54 +0530

    nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import {Navbar} from './Components/Navbar';
-import {MainRoute} from './Pages/MainRoute';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Project from './Pages/Projects';
+import Resume from './Pages/Resume';
+// import {MainRoute} from './Pages/MainRoute';
 
 function App() {
   return (
     <div>
      <Navbar/>
-     <MainRoute/>
+     <Home/>
+      <About/>
+      <Contact/>
+      <Project/>
+      <Resume/>
     </div>
   );
 }
